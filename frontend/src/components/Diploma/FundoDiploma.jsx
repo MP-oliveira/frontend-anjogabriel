277770f0b41b3a3b32109b4d93cb3825114470cf
Mauricio Oliveira <mau_oliver@hotmail.com>
2025-02-14 13:58:55 -0300

    fix: adjusting fundo diploma

--- frontend/src/components/Diploma/FundoDiploma.jsx
+++ frontend/src/components/Diploma/FundoDiploma.jsx
@@ -3,6 +3,7 @@
 import './FundoDiploma.css';
 
 const FundoDiploma = () => {
+
   return (
     <div className="diploma-container">
       <div className="page-border"></div>
@@ -48,10 +49,10 @@ const FundoDiploma = () => {
         {/* Perfil e Competências */}
         <section className="perfil-section">
           <p className="section-title">PERFIL PROFISSIONAL DO TÉCNICO DE ENFERMAGEM: </p>
-            <span className="section-text"> Prestar cuidados de enfermagem a individuo e população com a prerrogativa legal 
-              de assistir ao Enfermeiro nos diferentes níveis de atenção a saúde, em especial junto a pacientes graves e/ou 
-              com demandas especiais e no planejamento, programação orientação
-              e supervisão das atividades de assistência, com integrante da equipe.</span>
+          <span className="section-text"> Prestar cuidados de enfermagem a individuo e população com a prerrogativa legal
+            de assistir ao Enfermeiro nos diferentes níveis de atenção a saúde, em especial junto a pacientes graves e/ou
+            com demandas especiais e no planejamento, programação orientação
+            e supervisão das atividades de assistência, com integrante da equipe.</span>
 
         </section>
         <section className="competencia-section">
@@ -66,230 +67,324 @@ const FundoDiploma = () => {
         </section>
 
         {/* Tabelas de Módulos */}
+        {/* Tabelas de Módulos */}
         <section className="modulos-section">
-          {/* Módulo I */}
-          <div className="modulo">
-            <h3 className="modulo-title">Módulo I</h3>
-            <table className="modulo-table">
-              <thead>
-                <tr>
-                  <th>Componentes Curriculares</th>
-                  <th>Teoria/Prática</th>
-                  <th>Estágio</th>
-                  <th>Notas</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Anatomia e Fisiologia Humanas</td>
-                  <td>100</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Microbiologia e Parasitologia</td>
-                  <td>80</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Higiene e Profilaxia</td>
-                  <td>50</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Nutrição e Dietética</td>
-                  <td>50</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Psicologia Aplicada e Ética Profissional</td>
-                  <td>50</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr className="subtotal-row">
-                  <td>Sub-Total</td>
-                  <td>330</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <table className="modulo-table">
+            <tbody>
+              {/* Módulo I */}
+              <tr className="header-row">
+                <td >Módulo I</td>
+                <td>Carga Horária / Semestral</td>
+                <td className='no-border'>Autenticação da Escola</td>
+              </tr>
+              <tr>
+                <td>
+                  <table className="inner-table">
+                    <thead>
+                      <tr>
+                        <th>Componentes Curriculares</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr><td>Anatomia e Fisiologia Humanas</td></tr>
+                      <tr><td>Microbiologia e Parasitologia</td></tr>
+                      <tr><td>Higiene e Profilaxia</td></tr>
+                      <tr><td>Nutrição e Dietética</td></tr>
+                      <tr><td>Psicologia Aplicada e Ética Profissional</td></tr>
+                      <tr><td className="subtotal-row">Sub - Total</td></tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td>
+                  <table className="inner-table">
+                    <thead>
+                      <tr>
+                        <th>Teoria/ Prática</th>
+                        <th>Notas</th>
+                        <th>Estágio</th>
+                        <th>Notas</th>
+                      </tr>
+                    </thead>
+                    <tbody className='body-flex'>
+                      <tr>
+                        <td>100</td>
+                        <td></td>
+                        <td>-</td>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <td>80</td>
+                        <td></td>
+                        <td>-</td>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <td>50</td>
+                        <td></td>
+                        <td>-</td>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <td>50</td>
+                        <td></td>
+                        <td>-</td>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <td>50</td>
+                        <td></td>
+                        <td>-</td>
+                        <td>-</td>
+                      </tr>
+                      <tr className="subtotal-row">
+                        <td>330</td>
+                        <td></td>
+                        <td>-</td>
+                        <td>-</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td rowSpan="8" className="autenticacao-cell">
+                  <div className="linha-assinatura"></div>
+                  <p>Autenticação da Escola</p>
+                  <p className="codigo">CM CS:58/2018BA</p>
+                  <p>Campo Reservado ao COREN</p>
+                </td>
+              </tr>
 
-          {/* Módulo II */}
-          <div className="modulo">
-            <h3 className="modulo-title">Módulo II</h3>
-            <table className="modulo-table">
-              <thead>
-                <tr>
-                  <th>Componentes Curriculares</th>
-                  <th>Teoria/Prática</th>
-                  <th>Estágio</th>
-                  <th>Notas</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Farmacologia</td>
-                  <td>60</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Introdução a Enfermagem</td>
-                  <td>100</td>
-                  <td>100</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Noções de Administração na Unidade de Enfermagem</td>
-                  <td>50</td>
-                  <td>40</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Biossegurança</td>
-                  <td>50</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr className="subtotal-row">
-                  <td>Sub-Total</td>
-                  <td>260</td>
-                  <td>140</td>
-                  <td>-</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+              {/* Módulo II */}
+              <tr>
+                <th>Módulo II</th>
+              </tr>
+              <tr>
+                <td>
+                  <table className="inner-table">
+                    <thead>
+                      <tr>
+                        <th>Componentes Curriculares</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr><td>Farmacologia</td></tr>
+                      <tr><td>Introdução a Enfermagem</td></tr>
+                      <tr><td>Noções de Administração na Unidade de Enfermagem</td></tr>
+                      <tr><td>Biossegurança</td></tr>
+                      <tr><td className="subtotal-row">Sub - Total</td></tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td>
+                  <table className="inner-table">
+                    <thead>
+                      <tr>
+                        <th>Teoria/ Prática</th>
+                        <th>Notas</th>
+                        <th>Estágio</th>
+                        <th>Notas</th>
+                      </tr>
+                    </thead>
+                    <tbody className='body-flex'>
+                      <tr>
+                        <td>60</td>
+                        <td></td>
+                        <td>-</td>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <td>100</td>
+                        <td></td>
+                        <td>100</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>50</td>
+                        <td></td>
+                        <td>40</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>50</td>
+                        <td></td>
+                        <td>-</td>
+                        <td>-</td>
+                      </tr>
+                      <tr className="subtotal-row">
+                        <td>260</td>
+                        <td></td>
+                        <td>140</td>
+                        <td></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
 
-          {/* Módulo III */}
-          <div className="modulo">
-            <h3 className="modulo-title">Módulo III</h3>
-            <table className="modulo-table">
-              <thead>
-                <tr>
-                  <th>Componentes Curriculares</th>
-                  <th>Teoria/Prática</th>
-                  <th>Estágio</th>
-                  <th>Notas</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Enfermagem nas Emergências</td>
-                  <td>70</td>
-                  <td>70</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Enfermagem em Clínica Médica</td>
-                  <td>100</td>
-                  <td>100</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Enfermagem em Clínica Cirúrgica</td>
-                  <td>60</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Enfermagem Materno Infantil</td>
-                  <td>70</td>
-                  <td>50</td>
-                  <td>-</td>
-                </tr>
-                <tr className="subtotal-row">
-                  <td>Sub-Total</td>
-                  <td>300</td>
-                  <td>220</td>
-                  <td>-</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+              {/* Módulo III */}
+              <tr>
+                <th>
+                  Módulo III
+                </th>
+              </tr>
+              <tr>
+                <td>
+                  <table className="inner-table">
+                    <thead>
+                      <tr>
+                        <th>Componentes Curriculares</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr><td>Enfermagem nas Emergências</td></tr>
+                      <tr><td>Enfermagem em Clínica Médica</td></tr>
+                      <tr><td>Enfermagem em Clínica Cirúrgica</td></tr>
+                      <tr><td>Enfermagem Materno Infantil</td></tr>
+                      <tr><td className="subtotal-row">Sub - Total</td></tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td>
+                  <table className="inner-table">
+                    <thead>
+                      <tr>
+                        <th>Teoria/ Prática</th>
+                        <th>Notas</th>
+                        <th>Estágio</th>
+                        <th>Notas</th>
+                      </tr>
+                    </thead>
+                    <tbody className='body-flex'>
+                      <tr>
+                        <td>70</td>
+                        <td></td>
+                        <td>70</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>100</td>
+                        <td></td>
+                        <td>100</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>60</td>
+                        <td></td>
+                        <td>-</td>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <td>70</td>
+                        <td></td>
+                        <td>50</td>
+                        <td></td>
+                      </tr>
+                      <tr className="subtotal-row">
+                        <td>300</td>
+                        <td></td>
+                        <td>220</td>
+                        <td></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
 
-          {/* Módulo IV */}
-          <div className="modulo">
-            <h3 className="modulo-title">Módulo IV</h3>
-            <table className="modulo-table">
-              <thead>
-                <tr>
-                  <th>Componentes Curriculares</th>
-                  <th>Teoria/Prática</th>
-                  <th>Estágio</th>
-                  <th>Notas</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Enfermagem Pediátrica</td>
-                  <td>70</td>
-                  <td>50</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Enfermagem em Saúde Publica</td>
-                  <td>100</td>
-                  <td>90</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Enfermagem em Saúde Mental</td>
-                  <td>70</td>
-                  <td>50</td>
-                  <td>-</td>
-                </tr>
-                <tr>
-                  <td>Enfermagem em Geriatria</td>
-                  <td>70</td>
-                  <td>50</td>
-                  <td>-</td>
-                </tr>
-                <tr className="subtotal-row">
-                  <td>Sub-Total</td>
-                  <td>310</td>
-                  <td>240</td>
-                  <td>-</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+              {/* Módulo IV */}
+              <tr>
+                <th>
+                  Módulo IV
+                </th>
+              </tr>
+              <tr>
+                <td>
+                  <table className="inner-table">
+                    <thead>
+                      <tr>
+                        <th>Componentes Curriculares</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr><td>Enfermagem Pediátrica</td></tr>
+                      <tr><td>Enfermagem em Saúde Publica</td></tr>
+                      <tr><td>Enfermagem em Saúde Mental</td></tr>
+                      <tr><td>Enfermagem em Geriatria</td></tr>
+                      <tr><td className="subtotal-row">Sub-Total</td></tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td>
+                  <table className="inner-table">
+                    <thead>
+                      <tr>
+                        <th>Teoria/ Prática</th>
+                        <th>Notas</th>
+                        <th>Estágio</th>
+                        <th>Notas</th>
+                      </tr>
+                    </thead>
+                    <tbody className='body-flex'>
+                      <tr>
+                        <td>70</td>
+                        <td></td>
+                        <td>50</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>100</td>
+                        <td></td>
+                        <td>90</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>70</td>
+                        <td></td>
+                        <td>50</td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>70</td>
+                        <td></td>
+                        <td>50</td>
+                        <td></td>
+                      </tr>
+                      <tr className="subtotal-row">
+                        <td>310</td>
+                        <td></td>
+                        <td>240</td>
+                        <td></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
 
-          {/* Total Geral */}
-          <div className="modulo">
-            <table className="modulo-table">
-              <tbody>
-                <tr className="total-row">
-                  <td>Total Geral</td>
-                  <td>1200</td>
-                  <td>600</td>
-                  <td>-</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+              {/* Total Geral */}
+              <tr>
+                <td>
+                  <table className="inner-table">
+                    <tbody>
+                      <tr><td className="total-row">Total Geral</td></tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td>
+                  <table className="inner-table">
+                    <tbody>
+                      <tr className="total-row">
+                        <td>1200</td>
+                        <td></td>
+                        <td>600</td>
+                        <td></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </section>
-
-        {/* Rodapé */}
-        <footer className="diploma-footer">
-          <div className="autenticacao">
-            <div className="linha-assinatura"></div>
-            <p>Autenticação da Escola</p>
-            <p className="codigo">CM CS:58/2018BA</p>
-          </div>
-          <div className="coren">
-            <div className="linha-assinatura"></div>
-            <p>Campo Reservado ao COREN</p>
-          </div>
-        </footer>
       </div>
-    </div>
+    </div >
   );
 };
 
