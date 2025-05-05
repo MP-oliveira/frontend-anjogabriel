cb3e49c86e246c7ba2f258cc832665f95985d703
Mauricio Oliveira <mau_oliver@hotmail.com>
2025-05-05 10:13:50 -0300

    fix: adjust

--- backend/routes/diplomaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const diplomaController = require('../controllers/DiplomasController');
-
-
-router.post('/create', diplomaController.createDiploma);
-
-
-module.exports = router;
--- frontend/src/services/api.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// Configurando a URL base do backend
-const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
-});
-
-export default api;
